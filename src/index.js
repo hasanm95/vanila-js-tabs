d95fb82d598be0678da70e2db156b7e60776aaa1
hasanm95 <hsncool10@gmail.com>
2021-09-11 12:48:09 +0600

    completed tab

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 const tabs = document.querySelector(".nav-tabs");
-const tabBtns = tabs.querySelectorAll(".nav-link");
 const contents = document.querySelector(tabs.dataset["target"]);
 
 tabs.addEventListener("click", function (e) {
   const target = e.target;
   if (!target) return;
-  const parent = e;
   const targetTab = document.querySelector(target.dataset["target"]);
-  console.log(target);
+  const parent = e.target.parentElement.parentElement;
+  const allButton = parent.querySelectorAll("button")
+  const allPanes = contents.querySelectorAll(".tab-pane");
+  allButton.forEach((btn, i) => {
+    btn.classList.remove("active")
+    allPanes[i].classList.remove("active")
+  });
   targetTab.classList.add("active");
   target.classList.add("active");
 });
